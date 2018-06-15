9c9badd8f13ed6c39b7854a5ee5663b7ebc1d510
Pieter <pieter.pas@outlook.com>
2018-06-15 16:46:09 +0200

    Cleanup arrays

--- Examples/Coefficients.m
+++ Examples/Coefficients.m
@@ -1,38 +1,44 @@
 close all;
 
-frequencies = [63 117 216 399 737 1361 2513 4643 8576 15842];
+samplefreq = 44100;
+frequencies = [63 117 216 399 737 1361 2513 4643 8576 15842]; % 3dB corner frequencies
+order = 4;
 
 filters = [];
-d = fdesign.lowpass('N,F3dB',4,frequencies(1),44.1e3);
+d = fdesign.lowpass('N,F3dB',order,frequencies(1),samplefreq);
 filters = [filters design(d, 'butter')];
-
 for i = 1:9
-    d = fdesign.bandpass('N,F3dB1,F3dB2',4,frequencies(i),frequencies(i+1),44.1e3);
+    d = fdesign.bandpass('N,F3dB1,F3dB2',order,frequencies(i),frequencies(i+1),samplefreq);
     filters = [filters design(d,'butter')];
 end
-
-d = fdesign.highpass('N,F3dB',4,frequencies(end),44.1e3);
+d = fdesign.highpass('N,F3dB',order,frequencies(end),samplefreq);
 filters = [filters design(d, 'butter')];
 
 fvtool(filters);
 
+number_of_biquads = ceil(order/2);
+fprintf('const double sos_matrices[][%d][6] = {\n', number_of_biquads); 
 for i = 1:10
+    fprintf('    {\n');
     SOS_matrix = filters(i).coefficients{1,1};
-    number_of_biquads = size(SOS_matrix, 1);
-    fprintf('const double sos_matrix_%d[][6] = {\n', i); 
     for row = SOS_matrix.'
-        fprintf('    {');
+        fprintf('        {');
         for coeff = row
             fprintf('%.16e, ', coeff);
         end
         fprintf('},\n');
     end
-    fprintf('};\n');
-    gain_array = filters(i).coefficients{1,2};
-    fprintf('const double gain_array_%d[] = { ', i); 
-    for gain = gain_array(1:number_of_biquads)
+    fprintf('    },\n');
+end
+fprintf('};\n\n');
+
+fprintf('const double gain[][%d] = {\n', number_of_biquads); 
+for i = 1:10
+    fprintf('    {');
+    gain_array = filters(i).coefficients{1,2}(1:number_of_biquads);
+    for gain = gain_array
         fprintf('%.16e, ', gain);
     end
-    fprintf('};\n');
-    fprintf('SOSFilter<%d> filter_%d(sos_matrix_%d, gain_array_%d);\n\n', number_of_biquads , i, i, i);
+    fprintf('},\n');
 end
+fprintf('};\n\n');
